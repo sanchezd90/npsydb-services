e1a12f7a3d863f2852b937fc9f3b1a2ce83401ce
sanchezd90 <sanchezd90@gmail.com>
2021-08-28 17:02:23 -0300

    refactor basicQuery in order allow the use of same file as destination over multiple queries

--- NpsyScrapper.py
+++ NpsyScrapper.py
@@ -3,8 +3,7 @@ import json
 import time
 
 class NpsyScrapper():
-    def __init__(self):
-        self.results={}
+          
     #--File management--#
     def mergeDict(self,main,secondary,primaryKey,foreignKey):
         #merge dictionaries for pruebas and denominaciones
@@ -28,29 +27,48 @@ class NpsyScrapper():
             f.write(content)
     
     #--Query management--#
-    def basicQuery(self,source,idRange,sleep,*args):
+    def basicQuery(self,source,destination,idRange,sleep,*args):
         if type(idRange)!=list:
             idRange=[idRange,idRange+1]
+        print(type(idRange))
         with open(source,"r",encoding="utf-8") as f:
             content=f.read()
             pruebas=json.loads(content)
-        for prueba in pruebas:
-            for id in range(int(idRange[0]),int(idRange[1])):
-                if prueba['id']==id:
+        with open(destination,"r",encoding="utf-8") as f:
+            content=f.read()
+            results=json.loads(content)
+        for id in range(int(idRange[0]),int(idRange[1])):
+            print(id)
+            for prueba in pruebas:
+                print(prueba)
+                if prueba['id']==str(id):
                     for arg in args:
                         s=Search(f"{prueba['nombre_principal']} {arg}","title","npsydb","sanchezd90@gmail.com")                    
-                        self.results[f"{prueba['nombre_principal']} {arg}"]=s.getData(id,"all")
+                        if f"{prueba['nombre_principal']} {arg}" in results:                                                        
+                            results[f"{prueba['nombre_principal']} {arg}"].append(s.getData(id,"all"))
+                        else:
+                            results[f"{prueba['nombre_principal']} {arg}"]=s.getData(id,"all")
                     for arg in args:
                         s=Search(f"{prueba['acronym']} {arg}","title","npsydb","sanchezd90@gmail.com")                    
-                        self.results[f"{prueba['acronym']} {arg}"]=s.getData(id,"all")
+                        if f"{prueba['acronym']} {arg}" in results:
+                            results[f"{prueba['acronym']} {arg}"].append(s.getData(id,"all"))
+                        else:
+                            results[f"{prueba['acronym']} {arg}"]=s.getData(id,"all")
                     s=Search(f"{prueba['nombre_principal']}","title","npsydb","sanchezd90@gmail.com")                
-                    self.results[f"{prueba['nombre_principal']}"]=s.getData(id,"all")
+                    if f"{prueba['nombre_principal']}" in results:
+                        results[f"{prueba['nombre_principal']}"].append(s.getData(id,"all"))
+                    else:
+                        results[f"{prueba['nombre_principal']}"]=s.getData(id,"all")
                     s=Search(f"{prueba['acronym']}","title","npsydb","sanchezd90@gmail.com")                
-                    self.results[f"{prueba['acronym']}"]=s.getData(id,"all")
-            with open(f"{id}.json","w",encoding="utf-8") as f:
-                content=json.dumps(self.results,indent=4)
-                f.write(content)
+                    if f"{prueba['acronym']}" in results:
+                        results[f"{prueba['nombre_principal']}"].append(s.getData(id,"all"))
+                    else:
+                        results[f"{prueba['nombre_principal']}"]=s.getData(id,"all")
+            print("Time to sleep")
             time.sleep(sleep)
+        with open(destination,"w",encoding="utf-8") as f:
+            content=json.dumps(results,indent=4)
+            f.write(content)
 
-
-
+n=NpsyScrapper()
+n.basicQuery("pruebas.json","results.json",["239","240"],60,"norms","normative data")
